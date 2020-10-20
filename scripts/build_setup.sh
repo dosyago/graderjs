c7cb469613baef1bd00a607537d8c21a26fef3d5
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-10-20 14:50:22 +0000

    "TSC"

--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -1,11 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Installing nexe and upx..."
 
-npm i -g nexe
-curl -L -o upx.tar.xz https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
-tar -xJf upx.tar.xz
-rm upx.tar.xz
-sudo cp upx-3.96-amd64_linux/upx /usr/local/bin
-rm -rf upx-3.96-amd64_linux
+isnexe=$(which nexe)
+isupx=$(which upx)
+
+if [ -z "$isnexe" ]; then
+  npm i -g nexe
+else
+  echo "Nexe installed already."
+fi
+if [ -z "$isupx" ]; then
+  curl -L -o upx.tar.xz https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
+  tar -xJf upx.tar.xz
+  rm upx.tar.xz
+  sudo cp upx-3.96-amd64_linux/upx /usr/local/bin
+  rm -rf upx-3.96-amd64_linux
+else 
+  echo "upx installed already."
+fi
 
