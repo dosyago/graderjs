9a218258fcd8220426ac7c12c588573f30e58cb4
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-10-25 10:29:45 +0000

    "Good"

---
+++ build/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "GraderDemoApp",
+  author: {
+    name: "dosyago",
+    url: "https://github.com/dosyago",
+  },
+  desiredPort: 22121,
+  version: "0.0.1",
+  description: "A Beautiful Demonstration of Just a Tiny Fraction of The Amazing Benevolence Which Grader Hath To Offer",
+  source: "https://github.com/c9fe/grader",
+  organization: {
+    name: "Grader",
+    url: "https://github.com/grader-js"
+  }
+}
