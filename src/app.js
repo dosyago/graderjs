73cf147bb2dae3facbca7be882a25448c2841f21
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-10-29 09:53:19 +0000

    "OK"

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import Grader from './index.js'; 
-// import {windowDemo} from './demos.js';
+import {windowDemo} from './demos.js';
 
 start();
 
 async function start() {
-  await Grader.go();
+  //await Grader.go();
+  await windowDemo();
 }
 
 
